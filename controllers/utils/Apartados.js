1ed23230a54e3e1e9409a6f3da24379b55a0a678
20220270 <20220270@ricaldone.edu.sv>
2024-02-22 14:50:27 -0600

    Pantalla de administradores terminada

--- controllers/utils/Apartados.js
+++ controllers/utils/Apartados.js
@@ -3,7 +3,7 @@ HEADER.innerHTML = `
 <nav class="navbar navbar-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="actualizar_administradores.html">
-                <img src="../../resources/Imagenes/logo.png" alt="" width="80 px" height="80 px"
+                <img src="../../resources/Imagenes/logo.png" alt="" width="90 px" height="90 px"
                     class="d-inline-block ">
             </a>
             <div class="txtAdmin">
