996c1e1b7f3dca93d50e19ec02711edc0a3327f9
20220270 <20220270@ricaldone.edu.sv>
2024-02-22 13:49:52 -0600

    Archivo js creado para los aparttados

---
+++ controllers/utils/Apartados.js
@@ -0,0 +1,14 @@
+const HEADER = document.querySelector('nav');
+HEADER.innerHTML = `
+<nav class="navbar navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="actualizar_administradores.html">
+                <img src="../../resources/Imagenes/logo.png" alt="" width="80 px" height="80 px"
+                    class="d-inline-block ">
+            </a>
+            <div class="txtAdmin">
+                <h6>Admin</h6>
+            </div>
+        </div>
+    </nav>
+`;
---
+++ controllers/utils/recuperacionContra.js
@@ -0,0 +1,17 @@
+/*Código para tener la barra roja que esta encima de la pantalla, el logo de la empresa y el texto "Recuperación de contraseña"*/
+
+const HEADER = document.querySelector('header');
+HEADER.innerHTML = `
+<div class="red-bar" id="barra"></div>
+<div class="container text-center">
+    <div class="row">
+        <div class="col-sm-12 col-md-5">
+            <img src="../../resources/Imagenes/logo.png" width="200px" height="200px" alt="logo">
+        </div>
+        <div class="col-sm-12 col-md-7">
+            <h1>Recuperación de contraseña</h1>
+        </div>
+    </div>
+</div>
+<hr>
+`;
