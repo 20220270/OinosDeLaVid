00dd2f5fd16e7fc7b06817c3aaf54624c7838a09
20220270 <20220270@ricaldone.edu.sv>
2024-02-23 09:19:29 -0600

    Pantallas terminadas

--- controllers/admin/administrador.js
+++
@@ -1,7 +0,0 @@
-const openCreate = () => {
-    // Se muestra la caja de diálogo con su título.
-    SAVE_MODAL.show();
-    MODAL_TITLE.textContent = 'Crear categoría';
-    // Se prepara el formulario.
-    SAVE_FORM.reset();
-}
---
+++ controllers/utils/footer.js
@@ -0,0 +1,57 @@
+const FOOTER = document.querySelector('footer');
+FOOTER.innerHTML = `
+<footer>
+        <div class="container-fluid bg-dark text-light">
+            <div class="mt-3">
+                <br>
+                <h3 class="text center mt">Oinos de la Vid</h3>
+                <p>Recuerde hacer sus registro con precaución. Asegúrese de tener un buen
+                    manejo del sistema para evitar problemas con el mismo.
+                </p>
+
+                <hr>
+            </div>
+
+            <div class="row justify-content-around text-center text-md-start">
+                <div class="col-md-2 text-center">
+
+                    <img src="../../resources/Imagenes/logoBlanco.png" alt="logo" id="imagenLL">
+                </div>
+
+                <div class="col-md-3">
+
+                    <ul class="list-unstyled">
+                        <li class="fw-bold my-2">Redes sociales:</li>
+                        <li><a href="#" class="text-decoration-none text-white">
+                                <img src="../../resources/Imagenes/LogoTwiiter.png" alt="" id="LogoTwiiter">
+                                <img src="../../resources/Imagenes/LogoInsta.png" alt="" id="LogoTwiiter">
+                            </a>
+                        </li>
+                        <br>
+                        <br>
+                        Redes sociales de Oinos de la Vid
+                    </ul>
+                </div>
+
+                <div class="col-md-2">
+                    <ul class="list-unstyled">
+                        <li class="fw-bold my-2">Enlace a:</li>
+                        <li><a href="#">
+                                <img src="../../resources/Imagenes/irSitioP.png" alt="Sitio publico" id="irSitioP">
+                            </a></li>
+
+                    </ul>
+                </div>
+
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 text-center pt-2">
+                    <p>&copy; 2024 Copyright <a href="#" class="text-white">Oinos de la vid</a></p>
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
+`;
