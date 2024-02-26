3512d956e28d26480b5219a8f0539df0b3c312c9
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 22:46:11 -0600

    Cambio 7: Ya funcionan todas las alerts de exito en todas las vistas

--- controllers/admin/productos.js
+++ controllers/admin/productos.js
@@ -10,5 +10,42 @@ $(document).ready(function(){
     });
   });
 
-  
+  //Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
+$(document).ready(function () {
+  $("#btnGuardarD").click(function () {
+    $("#miModalR").modal("show");
+  });
+  $("#btnGuardarD").click(function () {
+    $("#miModalP").modal("hide");
+  });
+});
+
+  //Ventana modal para mostrar la actualización de datos
+$(document).ready(function () {
+  $("#btnAAActualiz").click(function () {
+    $("#miModalAC").modal("show");
+  });
+  $("#btnAAActualiz").click(function () {
+    $("#miModalP2").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
+  });
+});
+
+
+//Ventana modal para mostrar la eliminación de un datos
+
+$(document).ready(function () {
+  $("#btnElimin").click(function () {
+    $("#miModalDe2").modal("show");
+  });
+  $("#btnElimin").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
 
