5891d088bda74323527ea69e7128ca1b25a4de33
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 21:04:30 -0600

    cambios.3

--- controllers/admin/marcas.js
+++ controllers/admin/marcas.js
@@ -9,3 +9,34 @@ $(document).ready(function(){
       $("#miModalM2").modal("show");
     });
   });
+
+  $(document).ready(function(){
+    $("#btnGuardar").click(function(){
+      $("#miModalR").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnEliminar").click(function(){
+      $("#miModalDe").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnElim").click(function(){
+      $("#miModalDe2").modal("show");
+    });
+    $("#btnElim").click(function(){
+        $("#miModalDe").modal("hide");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnActualizarR").click(function(){
+      $("#miModalAC").modal("show");
+    });
+  });
+
+  document.getElementById("btnMostrar").addEventListener("click", function() {
+    window.location.href = "../../views/admin/productos.html";
+});
