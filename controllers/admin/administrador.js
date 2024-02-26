6c38e5eced9a40d9d6c4a3fe26be777d0fa0b095
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 20:00:19 -0600

    cambios.2

---
+++ controllers/admin/administrador.js
@@ -0,0 +1,21 @@
+/* aqui se muestra el formulario para registrar */
+
+$(document).ready(function(){
+    $("#btnAbrirModal").click(function(){
+      $("#miModal").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnActualizar").click(function(){
+      $("#miModal2").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnContra").click(function(){
+      $("#miModal3").modal("show");
+    });
+  });
+
+
---
+++ controllers/admin/marcas.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btnModalMarca").click(function(){
+      $("#miModalM").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnActualizar").click(function(){
+      $("#miModalP2").modal("show");
+    });
+  });
