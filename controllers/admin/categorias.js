e380a9160cd985c5f5c3d61e8ed9717da9d6432d
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 19:34:52 -0600

    Cambios

--- controllers/admin/categorias.js
+++ controllers/admin/categorias.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+    $("#btnAbrirModal").click(function(){
+      $("#miModal").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnActualizar").click(function(){
+      $("#miModal2").modal("show");
+    });
+  });
 
---
+++ controllers/admin/productos.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#btnModalAbrir").click(function(){
+      $("#miModalP").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btnActualizar").click(function(){
+      $("#miModalP2").modal("show");
+    });
+  });
+
+  
+
