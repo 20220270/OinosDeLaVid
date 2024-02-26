a48c798fd4fff5cc64c13a0d48032bbe3b2ee4a3
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 21:45:03 -0600

    cambios.4

--- controllers/admin/administrador.js
+++ controllers/admin/administrador.js
@@ -1,21 +1,92 @@
 /* aqui se muestra el formulario para registrar */
-
-$(document).ready(function(){
-    $("#btnAbrirModal").click(function(){
-      $("#miModal").modal("show");
-    });
+$(document).ready(function () {
+  $("#btnAbrirModal").click(function () {
+    $("#miModal").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnActualizar").click(function(){
-      $("#miModal2").modal("show");
-    });
-  });
+//Ventana modal para mostrar el formulario de actualizar datos
 
-  $(document).ready(function(){
-    $("#btnContra").click(function(){
-      $("#miModal3").modal("show");
-    });
+$(document).ready(function () {
+  $("#btnActualizar").click(function () {
+    $("#miModal2").modal("show");
   });
+});
+
+//Ventana modal para mostrar el formulario de cambiar la contraseña
+
+$(document).ready(function () {
+  $("#btnContra").click(function () {
+    $("#miModal3").modal("show");
+  });
+});
+
+//Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
+$(document).ready(function () {
+  $("#btnGuardar").click(function () {
+    $("#miModalR").modal("show");
+  });
+  $("#btnGuardar").click(function () {
+    $("#miModal").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
+  });
+});
+
+
+//Ventana modal para mostrar la eliminación de un datos
+
+$(document).ready(function () {
+  $("#btnElimin").click(function () {
+    $("#miModalDe2").modal("show");
+  });
+  $("#btnElimin").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
+  });
+});
+
+
+//Ventana modal para mostrar la eliminación de un datos
+
+$(document).ready(function () {
+  $("#btnElimin").click(function () {
+    $("#miModalDe2").modal("show");
+  });
+  $("#btnElimin").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la actualización de datos
+$(document).ready(function () {
+  $("#btnActuA").click(function () {
+    $("#miModalAC").modal("show");
+  });
+  $("#btnActuA").click(function () {
+    $("#miModal2").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la actualización de contraseña
+$(document).ready(function () {
+  $("#btnGuardarContraseña").click(function () {
+    $("#miModalAC").modal("show");
+  });
+  $("#btnGuardarContraseña").click(function () {
+    $("#miModal3").modal("hide");
+  });
+});
 
 
--- controllers/admin/marcas.js
+++ controllers/admin/marcas.js
@@ -1,42 +1,54 @@
-$(document).ready(function(){
-    $("#btnModalMarca").click(function(){
-      $("#miModalM").modal("show");
-    });
-  });
+//Ventana modal para agregar marcas
 
-  $(document).ready(function(){
-    $("#botonActu").click(function(){
-      $("#miModalM2").modal("show");
-    });
+$(document).ready(function () {
+  $("#btnModalMarca").click(function () {
+    $("#miModalM").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnGuardar").click(function(){
-      $("#miModalR").modal("show");
-    });
+//Ventana modal para actualizar marcas
+$(document).ready(function () {
+  $("#botonActu").click(function () {
+    $("#miModalM2").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnEliminar").click(function(){
-      $("#miModalDe").modal("show");
-    });
+//Ventana modal para mostrar la inserción de datos
+$(document).ready(function () {
+  $("#btnGuardar").click(function () {
+    $("#miModalR").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnElim").click(function(){
-      $("#miModalDe2").modal("show");
-    });
-    $("#btnElim").click(function(){
-        $("#miModalDe").modal("hide");
-    });
+
+//Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnActualizarR").click(function(){
-      $("#miModalAC").modal("show");
-    });
+
+//Ventana modal para mostrar la eliminacipon de un datos
+
+$(document).ready(function () {
+  $("#btnElim").click(function () {
+    $("#miModalDe2").modal("show");
   });
+  $("#btnElim").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
 
-  document.getElementById("btnMostrar").addEventListener("click", function() {
-    window.location.href = "../../views/admin/productos.html";
+//Ventana modal para mostrar la actualización de datos
+
+$(document).ready(function () {
+  $("#btnActualizarR").click(function () {
+    $("#miModalAC").modal("show");
+  });
+});
+
+//método para que un botón abra una ventana
+document.getElementById("btnMostrar").addEventListener("click", function () {
+  window.location.href = "../../views/admin/productos.html";
 });
