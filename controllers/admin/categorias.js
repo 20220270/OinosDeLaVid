450ba1a46b11fcd21a13906dd331c8b6f8a44882
20220270 <20220270@ricaldone.edu.sv>
2024-02-25 22:11:29 -0600

    cambiso 5

--- controllers/admin/categorias.js
+++ controllers/admin/categorias.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    $("#btnAbrirModal").click(function(){
+    $("#btnModalAbrir").click(function(){
       $("#miModal").modal("show");
     });
+
   });
 
   $(document).ready(function(){
@@ -9,4 +10,63 @@ $(document).ready(function(){
       $("#miModal2").modal("show");
     });
   });
+
+  //Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
+$(document).ready(function () {
+  $("#btnGuardar").click(function () {
+    $("#miModalR").modal("show");
+  });
+  $("#btnGuardar").click(function () {
+    $("#miModal").modal("hide");
+  });
+});
+
+  //Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
+  });
+});
+
+
+//Ventana modal para mostrar la eliminación de un datos
+
+$(document).ready(function () {
+  $("#btnElimin").click(function () {
+    $("#miModalDe2").modal("show");
+  });
+  $("#btnElimin").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la confirmación de un dato eliminado
+$(document).ready(function () {
+  $("#btnEliminar").click(function () {
+    $("#miModalDe").modal("show");
+  });
+});
+
+
+//Ventana modal para mostrar la eliminación de un datos
+
+$(document).ready(function () {
+  $("#btnElimin").click(function () {
+    $("#miModalDe2").modal("show");
+  });
+  $("#btnElimin").click(function () {
+    $("#miModalDe").modal("hide");
+  });
+});
+
+//Ventana modal para mostrar la actualización de datos
+$(document).ready(function () {
+  $("#btnActtu").click(function () {
+    $("#miModalAC").modal("show");
+  });
+  $("#btnActuA").click(function () {
+    $("#miModal2").modal("hide");
+  });
+});
+
 
