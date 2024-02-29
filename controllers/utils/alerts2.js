07764fd4736b1c0624f6c4c1f5041f0f158d316b
20220270 <20220270@ricaldone.edu.sv>
2024-02-29 15:00:42 -0600

    utlimo cam

---
+++ controllers/utils/alerts2.js
@@ -0,0 +1,4 @@
+    //método para que un botón abra una ventana
+    document.getElementById("btnIni").addEventListener("click", function () {
+        window.location.href = "../../views/admin/index.html";
+      });
