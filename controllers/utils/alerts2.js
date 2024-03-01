9c81e769c6d43a39048e2fecb94c541852b24850
20220270 <20220270@ricaldone.edu.sv>
2024-03-01 08:05:19 -0600

    cambios.vv1

--- controllers/utils/alerts2.js
+++
@@ -1,4 +0,0 @@
-    //método para que un botón abra una ventana
-    document.getElementById("btnIni").addEventListener("click", function () {
-        window.location.href = "../../views/admin/index.html";
-      });
