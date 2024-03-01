775b15f6aa6f2ecbba31ffe6597753344a8f30ac
edgarcia1723 <20220082@ricaldone.edu.sv>
2024-03-01 10:33:48 -0600

    Index js para inicio privado

---
+++ controllers/admin/index.js
@@ -0,0 +1,14 @@
+function login() {
+    var user, pass;
+
+    user = document.getElementById("textoAlias").value;
+    pass = document.getElementById("textoContra").value;
+
+    if (user == "Enrique" && pass == "12345") {
+        console.log(location.href)
+        alert('Datos correctos');
+        location.href = 'menu.html';
+    } else {
+        alert("Datos Incorrectos");
+    }
+}
--- controllers/admin/menu.js
+++ controllers/admin/menu.js
@@ -1,25 +1,25 @@
 
 //método para que un botón abra una ventana
 document.getElementById("btnProductos").addEventListener("click", function () {
-    window.location.href = "../../views/admin/productos.html";
+    location.href = "productos.html";
   });
   //método para que un botón abra una ventana
 document.getElementById("btnCatego").addEventListener("click", function () {
-    window.location.href = "../../views/admin/categoria.html";
+    location.href = "categoria.html";
   });
   //método para que un botón abra una ventana
 document.getElementById("btnMarcas").addEventListener("click", function () {
-    window.location.href = "../../views/admin/marcas.html";
+    location.href = "marcas.html";
   });
   //método para que un botón abra una ventana
 document.getElementById("btnClientes").addEventListener("click", function () {
-    window.location.href = "../../views/admin/clientes.html";
+    location.href = "clientes.html";
   });
   //método para que un botón abra una ventana
 document.getElementById("btnAdmins").addEventListener("click", function () {
-    window.location.href = "../../views/admin/administradores.html";
+    location.href = "administradores.html";
   });
     //método para que un botón abra una ventana
 document.getElementById("btnCerrarCe").addEventListener("click", function () {
-  window.location.href = "../../views/admin/index.html";
+  location.href = "index.html";
 });
