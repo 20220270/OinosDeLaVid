3d609506eff099cdb8847242bcffe7aab494a699
20220270 <20220270@ricaldone.edu.sv>
2024-03-07 10:33:51 -0600

    Pantallas adaptadas

--- controllers/utils/Apartados.js
+++ controllers/utils/Apartados.js
@@ -1,42 +1,33 @@
 const HEADER = document.querySelector('nav');
 HEADER.innerHTML = `
-<nav class="navbar navbar-light">
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="menu.html">
-                <img src="../../resources/Imagenes/logo.png" alt="" width="90 px" height="90 px"
-                    class="d-inline-block ">
-            </a>
-            <div class="txtAdmin">
-                <h6>Admin</h6>
+            <a class="navbar-brand" href="#">Cerrar sesion</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="productos.html">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="categoria.html">Categorias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="marcas.html">Marcas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="clientes.html">Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="administradores.html">Administradores</a>
+                    </li>
+
+                </ul>
             </div>
         </div>
     </nav>
-<div class="container col1">
-            <div class="row row-cols-2 row-cols-lg-3">
-
-            <div class="col-2 col-lg-2 col-md-1 col-sm-2 mt-2" id="btnCerrarCe">
-            <button type="button" class="btn btn-dark">
-                <img src="../../resources/Imagenes/cerrarS.png" id="imagenC" alt="" height="20px" width="20px">
-                Cerrar sesión</button>
-        </div>
-
-                <div class="col-2 col-lg-2 col-md-2 col-sm-2 mt-3" id="btnProductos">
-                    <button type="button" class="btn btn-dark">Productos</button>
-                </div>
-
-                <div class="col-2 col-lg-2 col-md-2 col-sm-2 mt-3" id="btnCatego">
-                    <button type="button" class="btn btn-dark">Categorias</button>
-                </div>
-                <div class="col-1 col-lg-2 col-md-2 col-sm-1 mt-3" id="btnMarcas">
-                    <button type="button" class="btn btn-dark">Marcas</button>
-                </div>
-                <div class="col-1 col-lg-2 col-md-2 col-sm-2 mt-3" id="btnClientes">
-                    <button type="button" class="btn btn-dark">Clientes</button>
-                </div>
-                <div class="col-2 col-lg-1 col-md-1 mt-3" id="btnAdmins">
-                    <button type="button" class="btn btn-dark">Administradores</button>
-                </div>
-
-            </div>
-        </div>
 `;
---
+++ controllers/utils/ApartadosPublico.js
@@ -0,0 +1,15 @@
+const HEADER = document.querySelector('nav');
+HEADER.innerHTML = `
+<nav class="navbar navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="menu.html">
+                <img src="../../resources/Imagenes/logo.png" alt="" width="90 px" height="90 px"
+                    class="d-inline-block ">
+            </a>
+            <div class="txtAdmin">
+                <h6>ivanSalgue</h6>
+            </div>
+        </div>
+    </nav>
+
+`;
