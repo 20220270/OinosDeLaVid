ee577a84560f76e3129f431bdc1d4a3248b9ee2e
20220270 <20220270@ricaldone.edu.sv>
2024-03-07 11:53:37 -0600

    mod

---
+++ controllers/utils/footerPublico.js
@@ -0,0 +1,67 @@
+const FOOTERP = document.querySelector('footer');
+
+FOOTERP.innerHTML = `
+<div class="container-fluid bg-dark text-light">
+            <div class="mt-3">
+                <h3 class="text center">Aca pondremos algun textito</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dignissimos commodi doloribus repellendus
+                    iusto maiores, esse, beatae eligendi quod non, fuga perspiciatis earum qui sit dolor aliquid
+                    consequuntur ullam culpa?</p>
+                <hr>
+            </div>
+    
+            <div class="row justify-content-around text-center text-md-start">
+                <div class="col-md-2 text-center">
+    
+                    <h1 class="fw-bold mt-3">TP</h1>
+                    <h4>aca ira logo y nombre de la empresa</h4>
+                </div>
+    
+                <div class="col-md-3">
+    
+                    <ul class="list-unstyled">
+                        <li class="fw-bold my-2">partners</li>
+                        <li><a href="#" class="text-decoration-none text-white">website</a></li>
+                        <li><a href="#" class="text-decoration-none text-white">social media</a></li>
+                        <li><a href="#" class="text-decoration-none text-white">otra coas</a></li>
+                    </ul>
+                </div>
+    
+                <div class="col-md-2">
+                    <ul class="list-unstyled">
+                        <li class="fw-bold my-2">anosotros</li>
+                        <li><a href="#" class="text-decoration-none text-white">alguna otra cosita</a></li>
+                        <li><a href="#" class="text-decoration-none text-white">again otra cosita</a></li>
+                        <li><a href="#" class="text-decoration-none text-white">alguna otra cosita2</a></li>
+                    </ul>
+                </div>
+    
+                <div class="col-md-2">
+    
+                    <ul class="list-unstyled">
+                        <li class="fw-bold my-2">soporte tec</li>
+                        <li> <a href="#" class="text-decoration-none text-white">contactanos</a></li>
+                        <li> <a href="#" class="text-decoration-none text-white">jasfksaf</a></li>
+                    </ul>
+                </div>
+    
+                <div class="col-md-2">
+                    <li class="fw-bold my-2 list-unstyled">follow us</li>
+                    <ul class="list-unstyled d-flex justify-content-center justify-content-md-start">
+                        <li><a href="#" class="text-white"><i class="fa-brands fa-facebook me-2 fa-1x"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fa-brands fa-whatsapp mx-2 fa-1x"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fa-brands fa-youtube me-2 fa-1x"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fa-brands fa-instagram me-2 fa-1x"></i></a></li>
+                    </ul>
+                </div>
+    
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 text-center pt-2">
+                    <p>&copy; 2024 Copyright <a href="#" class="text-white">Luxury Wines</a></p>
+                </div>
+            </div>
+    
+        </div>
+`;
