86c74c67d99c92d843693b94450f364a1cbc3ff2
20220270 <20220270@ricaldone.edu.sv>
2024-03-10 21:31:08 -0600

    Carrito y mas cositas

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,6 +1,39 @@
 //agregar cantidad al carrito
 
+//agregar cantidad al carrito y actualizarlo al agregar mas productos
+
 document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.btnAgregarAlCarrito');
+  const textoCarrito = document.getElementById('textoCar');
+  const inputsCantidad = document.querySelectorAll('input[type="number"]');
+
+  let cantidadCarrito = 0;
+
+  buttons.forEach((button, index) => {
+      button.addEventListener('click', function() {
+          alert("Su producto ha sido registrado correctamente");
+          cantidadCarrito += parseInt(inputsCantidad[index].value); // Sumar la cantidad del input al carrito
+          actualizarTextoCarrito();
+      });
+  });
+
+  function actualizarTextoCarrito() {
+      textoCarrito.innerText = `Ver carrito (${cantidadCarrito})`;
+  }
+
+  // Actualizar el texto del carrito cuando se cambie la cantidad desde los inputs
+  inputsCantidad.forEach(input => {
+      input.addEventListener('input', function() {
+          cantidadCarrito = 0;
+          inputsCantidad.forEach(input => {
+              cantidadCarrito += parseInt(input.value);
+          });
+          actualizarTextoCarrito();
+      });
+  });
+});
+
+/*document.addEventListener('DOMContentLoaded', function() {
   const buttons = document.querySelectorAll('.btnAgregarAlCarrito');
   const textoCarrito = document.getElementById('textoCar');
 
@@ -13,12 +46,33 @@ document.addEventListener('DOMContentLoaded', function() {
           textoCarrito.innerText = `Ver carrito (${cantidadCarrito})`;
       });
   });
+});*/
+
+document.getElementById("textoCar").addEventListener("click", function () {
+  window.location.href = "carrito.html";
 });
 
 document.getElementById("btnVerPerfil").addEventListener("click", function () {
   window.location.href = "perfil.html";
 });
 
-document.getElementById("botonCarrito").addEventListener("click", function () {
-    window.location.href = "carrito.html";
-  });
+
+    document.querySelectorAll('.card').forEach(card => {
+        const input = card.querySelector('input[type="number"]');
+        const incrementBtn = card.querySelector('#incrementBtn');
+        const decrementBtn = card.querySelector('#decrementBtn');
+        const toast = document.getElementById('toast');
+
+        incrementBtn.addEventListener('click', () => {
+            input.stepUp();
+        });
+
+        decrementBtn.addEventListener('click', () => {
+            if (input.value > 0) {
+                input.stepDown();
+            } else {
+                const bsToast = new bootstrap.Toast(toast);
+                bsToast.show();
+            }
+        });
+    });
