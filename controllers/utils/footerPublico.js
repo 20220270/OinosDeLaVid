c7e161930d5830e24c204baa52e5dcb2e521f1d7
20220270 <20220270@ricaldone.edu.sv>
2024-03-12 10:09:32 -0600

    footer mejorado

--- controllers/utils/footerPublico.js
+++ controllers/utils/footerPublico.js
@@ -4,14 +4,105 @@ FOOTERP.innerHTML = `
 <div class="container-fluid bg-dark text-light">
 
         <div class="container-fluid bg-dark text-light">
-            <div class="mt-3">
-                <br>
-                <h3 class="text-center">Oinos de la Vid</h3>
-                <p class="text-center">
-                    No olvides recomendar nuestra tienda en línea ¡a todos!
+            
+            <!--Apartado para frases célebres-->
+        <div id="frases" class="carousel slide mt-5 bg-dark text-white mb-3" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active mt-5 mb-5">
+                <p class="text-center mt-3 fs-4">
+                    «El vino lava nuestras inquietudes,
+                    enjuaga el alma hasta el fondo y asegura la curación de la tristeza.» 
                 </p>
+                <p class="text-center fw-bold fs-5">
+                    Lucio Anneo Séneca.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-5">
+                    «El vino, mientras más se envejece,
+                    más calor tiene: al contrario de nuestra naturaleza, que mientras más vive, más se va enfriando.»
+                    
+                </p>
+                <p class="text-center fw-bold fs-5">
+                    Félix Lope de Vega y Carpio.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-2">
+                    «En el vino (está) la verdad». 
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Plinio «el viejo».
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-2">
+                    «Vino y pan movieron ejércitos.» 
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Juan Abellán.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-3">
+                    “El lenguaje es vino en los labios.” 
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Virginia Woolf.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center fs-4">
+                    “Hay más filosofía y sabiduría en una botella de vino, que en todos los libros.” 
+                </p>
+                <p class="text-center fw-bold fs-4">
+                    Louis Pasteur.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-3">
+                    “El vino es poesía embotellada.” 
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Robert Louis Stevenson.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-4">
+                    “El vino puede ser mejor profesor que la tinta, y charlar es a menudo mejor que los libros.” 
+                </p>
+                <p class="text-center fw-bold fs-4">
+                    Stephen Fry.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-3">
+                    “Donde no hay vino no hay amor.” 
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Eurípides.
+                </p>
+            </div>
+            <div class="carousel-item mt-5 mb-5">
+                <p class="text-center mt-3 fs-3">
+                    «El que al mundo vino y no toma vino, ¿a qué vino?”
+                </p>
+                <p class="text-center fw-bold fs-3">
+                    Bernardo Piuma.
+                </p>
+            </div>
+        </div>
 
-                <hr>
+        <button class="carousel-control-prev mt-5" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next mt-5" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!--Fin de apartado de frases célebres-->
             </div>
 
             <div class="row justify-content-around text-center text-md-start">
