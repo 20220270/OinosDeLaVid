94b56c71955b65dbf831411a7626beb781feb640
20220270 <20220270@ricaldone.edu.sv>
2024-03-20 10:25:08 -0600

    cambiooou

--- controllers/public/login.js
+++ controllers/public/login.js
@@ -1,4 +1,3 @@
-/*Declaracion y obtencion de variables del inicio privado*/
 function login() {
     var user, pass;
 
@@ -8,10 +7,12 @@ function login() {
     if (user == "Enrique" && pass == "12345") {
         console.log(location.href)
         alert('Datos correctos');
-        location.href = 'menu.html';
+        location.href = 'index.html';
     } else {
         alert("Datos Incorrectos");
     }
 }
 
-
+document.getElementById("btnIngresar").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
