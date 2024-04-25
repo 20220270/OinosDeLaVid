f8d57fd18213f4f8d2cc4cb548425439182ea922
20220270 <136751152+20220270@users.noreply.github.com>
2024-04-25 15:18:21 -0600

    s

--- controllers/admin/marcas.js
+++ controllers/admin/marcas.js
@@ -1,33 +1,11 @@
 //Codigo para redireccionar el perfil del administrador a la ventana anterior
 localStorage.setItem('paginaOrigen', window.location.href);
 
-//Ventana modal para agregar marcas
-
-$(document).ready(function () {
-  $("#btnModalMarca").click(function () {
-    $("#miModalM").modal("show");
-  });
-});
-
-//Ventana modal para actualizar marcas
-$(document).ready(function () {
-  $("#botonActu").click(function () {
-    $("#miModalM2").modal("show");
-  });
-});
 
 //Ventana modal para mostrar la inserción de datos
 $(document).ready(function () {
   $("#btnGuardar").click(function () {
     $("#miModalR").modal("show");
-  });
-});
-
-
-//Ventana modal para mostrar la confirmación de un dato eliminado
-$(document).ready(function () {
-  $("#btnEliminar").click(function () {
-    $("#miModalDe").modal("show");
   });
 });
 
@@ -51,7 +29,3 @@ $(document).ready(function () {
   });
 });
 
-//método para que un botón abra una ventana
-document.getElementById("btnMostrar").addEventListener("click", function () {
-  window.location.href = "../../views/admin/productos.html";
-});
