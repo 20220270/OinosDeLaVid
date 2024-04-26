38dc29817f5a4a39677cf0852408bb5b810f6c7f
20220270 <136751152+20220270@users.noreply.github.com>
2024-04-26 13:58:39 -0600

    ma

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -10,3 +10,13 @@ addForm.addEventListener("submit", (e)=>{
 })
 
 
+const addForm1 = document.getElementById("FormValidacion");
+
+addForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if(addForm.checkValidity() === false){
+        e.stopPropagation();
+        addForm.classList.add('was-validated');
+        return false
+    }
+})
