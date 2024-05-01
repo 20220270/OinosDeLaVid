6287ee1fe20f46679c697b0ae03dff489201d196
edgarcia1723 <20220082@ricaldone.edu.sv>
2024-05-01 11:38:45 -0600

    Connexion a la base de datos

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,22 +1,19 @@
 const addForm = document.getElementById("ValidacionInicio");
+const USER_API = 'services/admin/administrador.php';
 
-addForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if(addForm.checkValidity() === false){
-        e.stopPropagation();
-        addForm.classList.add('was-validated');
-        return false
+addForm.addEventListener("submit", async(e)=>{
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(LOGIN_FORM);
+    // Petición para iniciar sesión.
+    const DATA = await fetchData(USER_API, 'logIn', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'dashboard.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
     }
 })
 
 
-const addForm1 = document.getElementById("FormValidacion");
-
-addForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if(addForm.checkValidity() === false){
-        e.stopPropagation();
-        addForm.classList.add('was-validated');
-        return false
-    }
-})
