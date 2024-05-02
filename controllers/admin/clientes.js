72c85322c580b29291d3688fc809bbceea282180
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-02 11:54:47 -0600

    barras de busqeuda

--- controllers/admin/clientes.js
+++ controllers/admin/clientes.js
@@ -1,3 +1,29 @@
+//Busqueda de datos dentro de la card
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("Search");
+    const noResultsElement = document.getElementById("noResults"); // Referencia al elemento del mensaje
+
+    searchInput.addEventListener("input", function() {
+        const searchText = searchInput.value.toLowerCase();
+        const cards = document.querySelectorAll(".card");
+        let found = false; // Indicador de si se encontró al menos una coincidencia
+
+        cards.forEach(card => {
+            const cardText = card.textContent.toLowerCase();
+            if (cardText.includes(searchText)) {
+                card.style.display = "";
+                found = true; // Marca como encontrada una coincidencia
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+        // Muestra u oculta el mensaje de 'no resultados' basado en si se encontraron coincidencias
+        noResultsElement.style.display = found ? "none" : "block";
+    });
+});
+
 
 document.addEventListener("DOMContentLoaded", function() {
     var estadoCliente = document.getElementById("estadoCliente");
