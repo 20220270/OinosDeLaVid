bf2ebad11fa7c7f8b5239e03c0de0f0699d760db
edgarcia1723 <20220082@ricaldone.edu.sv>
2024-05-03 13:39:28 -0600

    inisio de secion completo y funcional

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,19 +1,20 @@
-const addForm = document.getElementById("ValidacionInicio");
+const addForm = document.getElementById('sessionForm');
 const USER_API = 'services/admin/administrador.php';
 
-addForm.addEventListener("submit", async(e)=>{
+addForm.addEventListener('submit', async(event)=>{
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
     // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(LOGIN_FORM);
+    const FORM = new FormData(addForm);
     // Petición para iniciar sesión.
     const DATA = await fetchData(USER_API, 'logIn', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
-        sweetAlert(1, DATA.message, true, 'dashboard.html');
+        sweetAlert(1, DATA.message, true, 'Menu.html');
     } else {
         sweetAlert(2, DATA.error, false);
     }
 })
+
 
 
