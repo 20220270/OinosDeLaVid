5dddd930a3c2d4f113052c9cbebe5a145e2bc59e
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-03 15:04:54 -0600

    commmit de cambios

--- controllers/utils/Apartados.js
+++ controllers/utils/Apartados.js
@@ -3,7 +3,7 @@ HEADER.innerHTML = `
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Cerrar sesion</a>
+            <a class="navbar-brand" href="categoria.html">Cerrar sesion</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
