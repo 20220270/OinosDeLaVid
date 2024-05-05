bd23f2542ddb3510296be1050106c0743af1872f
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-04 18:36:45 -0600

    cambios

--- controllers/admin/categorias.js
+++ controllers/admin/categorias.js
@@ -1,20 +1,170 @@
+// Constante para completar la ruta de la API.
+const CATEGORIA_API = 'services/admin/categoria.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer los elementos de la tabla.
+const TABLE_BODY = document.getElementById('tableBody'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_CATEGORIA = document.getElementById('idCategoria'),
+    NOMBRE_CATEGORIA = document.getElementById('nombreCategoria'),
+    DESCRIPCION_CATEGORIA = document.getElementById('descripcionCategoria'),
+    IMAGEN_CATEGORIA = document.getElementById('inputImgCat');
 
-        localStorage.setItem('paginaOrigen', window.location.href);
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Gestionar categorías';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
 
-$(document).ready(function(){
-    $("#btnModalAbrir").click(function(){
-      $("#miModal").modal("show");
-    });
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+  // Se evita recargar la página web después de enviar el formulario.
+  event.preventDefault();
+  // Constante tipo objeto con los datos del formulario.
+  const FORM = new FormData(SEARCH_FORM);
+  // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+  fillTable(FORM);
+});
 
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+  // Se evita recargar la página web después de enviar el formulario.
+  event.preventDefault();
+  // Se verifica la acción a realizar.
+  (ID_CATEGORIA.value) ? action = 'updateRow' : action = 'createRow';
+  // Constante tipo objeto con los datos del formulario.
+  const FORM = new FormData(SAVE_FORM);
+  // Petición para guardar los datos del formulario.
+  const DATA = await fetchData(CATEGORIA_API, action, FORM);
+  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+  if (DATA.status) {
+      // Se cierra la caja de diálogo.
+      SAVE_MODAL.hide();
+      // Se muestra un mensaje de éxito.
+      sweetAlert(1, DATA.message, true);
+      // Se carga nuevamente la tabla para visualizar los cambios.
+      fillTable();
+  } else {
+      sweetAlert(2, DATA.error, false);
+  }
+});
+
+const fillTable = async (form = null) => {
+  // Se inicializa el contenido de la tabla.
+  ROWS_FOUND.textContent = '';
+  TABLE_BODY.innerHTML = '';
+  // Se verifica la acción a realizar.
+  (form) ? action = 'searchRows' : action = 'readAll';
+  // Petición para obtener los registros disponibles.
+  const DATA = await fetchData(CATEGORIA_API, action, form);
+  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+  if (DATA.status) {
+      console.log(DATA)
+      // Se recorre el conjunto de registros fila por fila.
+      DATA.dataset.forEach(row => {
+          // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+          TABLE_BODY.innerHTML += `
+              <tr>
+                  <td><img src="${SERVER_URL}images/categorias/${row.imagen_categoria}" height="50"></td>
+                  <td>${row.nombre_categoria}</td>
+                  <td>${row.descripcion_categoria}</td>
+                  <td>
+                      <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_categoria})">
+                          <i class="bi bi-pencil-fill"></i>
+                      </button>
+                      <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_categoria})">
+                          <i class="bi bi-trash-fill"></i>
+                      </button>
+                      <button type="button" class="btn btn-warning" onclick="openReport(${row.id_categoria})">
+                          <i class="bi bi-filetype-pdf"></i>
+                      </button>
+                  </td>
+              </tr>
+          `;
+      });
+      // Se muestra un mensaje de acuerdo con el resultado.
+      ROWS_FOUND.textContent = DATA.message;
+  } else {
+      sweetAlert(4, DATA.error, true);
+  }
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+  // Se define una constante tipo objeto con los datos del registro seleccionado.
+  const FORM = new FormData();
+  FORM.append('idCategoria', id);
+  // Petición para obtener los datos del registro solicitado.
+  const DATA = await fetchData(CATEGORIA_API, 'readOne', FORM);
+  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+  if (DATA.status) {
+      // Se muestra la caja de diálogo con su título.
+      SAVE_MODAL.show();
+      MODAL_TITLE.textContent = 'Actualizar categoría';
+      // Se prepara el formulario.
+      SAVE_FORM.reset();
+      // Se inicializan los campos con los datos.
+      const ROW = DATA.dataset;
+      ID_CATEGORIA.value = ROW.id_categoria;
+      NOMBRE_CATEGORIA.value = ROW.nombre_categoria;
+      DESCRIPCION_CATEGORIA.value = ROW.descripcion_categoria;
+  } else {
+      sweetAlert(2, DATA.error, false);
+  }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+  // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+  const RESPONSE = await confirmAction('¿Desea eliminar la categoría de forma permanente?');
+  // Se verifica la respuesta del mensaje.
+  if (RESPONSE) {
+      // Se define una constante tipo objeto con los datos del registro seleccionado.
+      const FORM = new FormData();
+      FORM.append('idCategoria', id);
+      // Petición para eliminar el registro seleccionado.
+      const DATA = await fetchData(CATEGORIA_API, 'deleteRow', FORM);
+      // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+      if (DATA.status) {
+          // Se muestra un mensaje de éxito.
+          await sweetAlert(1, DATA.message, true);
+          // Se carga nuevamente la tabla para visualizar los cambios.
+          fillTable();
+      } else {
+          sweetAlert(2, DATA.error, false);
+      }
+  }
+}
+
+
+localStorage.setItem('paginaOrigen', window.location.href);
+
+
+
+$(document).ready(function () {
+  $("#btnActualizar").click(function () {
+    $("#miModal2").modal("show");
   });
+});
 
-  $(document).ready(function(){
-    $("#btnActualizar").click(function(){
-      $("#miModal2").modal("show");
-    });
-  });
-
-  //Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
+//Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
 $(document).ready(function () {
   $("#btnGuardar").click(function () {
     $("#miModalR").modal("show");
@@ -24,7 +174,7 @@ $(document).ready(function () {
   });
 });
 
-  //Ventana modal para mostrar la confirmación de un dato eliminado
+//Ventana modal para mostrar la confirmación de un dato eliminado
 $(document).ready(function () {
   $("#btnEliminar").click(function () {
     $("#miModalDe").modal("show");
@@ -72,15 +222,15 @@ $(document).ready(function () {
 });
 
 //Validación para que solamente se escriban letras, pero que pueda haber guiones y espacios
-$(document).ready(function() {
-  $("#nombreProducto").on("keypress", function(event) {
-      var inputValue = event.which;
-      // Permitir solo letras y algunos caracteres especiales como espacio, guión, etc.
-      if (!(inputValue >= 65 && inputValue <= 90) && // letras mayúsculas
-          !(inputValue >= 97 && inputValue <= 122) && // letras minúsculas
-          !(inputValue == 32 || inputValue == 45 || inputValue == 46 || inputValue == 44)) { // espacio, guión, punto, coma
-          event.preventDefault();
-      }
+$(document).ready(function () {
+  $("#nombreProducto").on("keypress", function (event) {
+    var inputValue = event.which;
+    // Permitir solo letras y algunos caracteres especiales como espacio, guión, etc.
+    if (!(inputValue >= 65 && inputValue <= 90) && // letras mayúsculas
+      !(inputValue >= 97 && inputValue <= 122) && // letras minúsculas
+      !(inputValue == 32 || inputValue == 45 || inputValue == 46 || inputValue == 44)) { // espacio, guión, punto, coma
+      event.preventDefault();
+    }
   });
 });
 
