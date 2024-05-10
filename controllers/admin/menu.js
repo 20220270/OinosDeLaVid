4a13437c44906b4d167148e389d5a37f1f830fd5
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-09 20:23:29 -0600

    Cambios

--- controllers/admin/menu.js
+++ controllers/admin/menu.js
@@ -28,30 +28,3 @@
 localStorage.setItem('paginaOrigen', window.location.href);
 
 
-document.getElementById("btnProductos").addEventListener("click", function () {
-  location.href = "productos.html";
-});
-//método para que un botón abra una ventana
-document.getElementById("btnProductos").addEventListener("click", function () {
-    location.href = "productos.html";
-  });
-  //método para que un botón abra una ventana
-document.getElementById("btnCatego").addEventListener("click", function () {
-    location.href = "categoria.html";
-  });
-  //método para que un botón abra una ventana
-document.getElementById("btnMarcas").addEventListener("click", function () {
-    location.href = "marcas.html";
-  });
-  //método para que un botón abra una ventana
-document.getElementById("btnClientes").addEventListener("click", function () {
-    location.href = "clientes.html";
-  });
-  //método para que un botón abra una ventana
-document.getElementById("btnAdmins").addEventListener("click", function () {
-    location.href = "administradores.html";
-  });
-    //método para que un botón abra una ventana
-document.getElementById("btnCerrarCe").addEventListener("click", function () {
-  location.href = "index.html";
-});
--- controllers/admin/productos.js
+++ controllers/admin/productos.js
@@ -1,106 +1,222 @@
+// Constantes para completar las rutas de la API.
+const PRODUCTO_API = 'services/admin/producto.php';
+const CATEGORIA_API = 'services/admin/categoria.php';
+const MARCA_API = 'services/admin/marca.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer el contenido de la tabla.
+const TABLE_BODY = document.getElementById('tableBody'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_PRODUCTO = document.getElementById('idProducto'),
+    CATEGORIA_PRODUCTO = document.getElementById('selectCategoria');
+    MARCA_PRODUCTO = document.getElementById('selectMarca');
+    NOMBRE_PRODUCTO = document.getElementById('nombreProducto'),
+    DESCRIPCION_PRODUCTO = document.getElementById('descripcionProducto'),
+    PRECIO_PRODUCTO = document.getElementById('PrecioProducto'),
+    IMAGEN_PRODUCTO = document.getElementById('inputImgProducto'),
+    ESTADO_PRODUCTO = document.getElementById('selectEstado');
+    CANTIDAD_PRODUCTO = document.getElementById('cantidadProducto'),
+    DESCUENTO_PRODUCTO = document.getElementById('descuentoProducto'),
+
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    //loadTemplate();
+    // Se establece el título del contenido principal.
+    //MAIN_TITLE.textContent = 'Gestionar productos';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
+
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_PRODUCTO.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(PRODUCTO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+const fillTable = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND.textContent = '';
+    TABLE_BODY.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(PRODUCTO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se establece un icono para el estado del producto.
+            (row.estado_producto) ? icon = 'bi bi-eye-fill' : icon = 'bi bi-eye-slash-fill';
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY.innerHTML += `
+                <tr>
+                <td>${row.id_producto}</td>
+                    <td>${row.nombre_categoria}</td>
+                    <td>${row.nombre_marca}</td>
+                    <td>${row.nombre_producto}</td>
+                    <td>${row.descripcion_producto}</td>
+                    <td>${row.precio_producto}</td>
+                    <td>${row.imagen_producto}</td>
+                    <td>${row.estado_producto}</td>
+                    <td>${row.existencias_producto}</td>
+                    <td>${row.descuento_producto}</td>
+                    <td><i class="${icon}"></i></td>
+                    <td>
+                        <button type="button" class="btn" onclick="openUpdate(${row.id_producto})">
+                            
+                            <img src="../../resources/Imagenes/btnActualizarIMG.png" alt="" width="30px" height="30px"
+                            class="mb-1">
+                        </button>
+                        <button type="button" class="btn" onclick="openDelete(${row.id_producto})">
+                        <img src="../../resources/Imagenes/btnEliminarIMG.png" alt="" width="30px" height="30px"
+                        class="mb-1">
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Crear producto';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+    ESTADO_PRODUCTO.disabled = false;
+    fillSelect(CATEGORIA_API, 'readAll', 'selectCategoria');
+    fillSelect(MARCA_API, 'readAll', 'selectMarca');
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    // Se define un objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idProducto', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(PRODUCTO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar producto';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        ESTADO_PRODUCTO.disabled = false;
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_PRODUCTO.value = ROW.id_producto;
+        NOMBRE_PRODUCTO.value = ROW.nombre_producto;
+        DESCRIPCION_PRODUCTO.value = ROW.descripcion_producto;
+        PRECIO_PRODUCTO.value = ROW.precio_producto;
+        ESTADO_PRODUCTO.checked = ROW.estado_producto;
+        CANTIDAD_PRODUCTO.value = ROW.existencias_producto;
+        DESCUENTO_PRODUCTO.value = ROW.descuento_producto;
+        
+        
+        fillSelect(CATEGORIA_API, 'readAll', 'selectCategoria', ROW.id_categoria);
+        fillSelect(MARCA_API, 'readAll', 'selectMarca', ROW.id_marca);
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el producto de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idProducto', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(PRODUCTO_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
+
+/*
+*   Función para abrir un reporte automático de productos por categoría.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+/*const openReport = () => {
+    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
+    const PATH = new URL(`${SERVER_URL}reports/admin/productos.php`);
+    // Se abre el reporte en una nueva pestaña.
+    window.open(PATH.href);
+}*/
+
+
+
+
+
+
 //Codigo para redireccionar el perfil del administrador a la ventana anterior
 localStorage.setItem('paginaOrigen', window.location.href);
-
-
-
-  //Ventana modal para mostrar la inserción de datos y ocultar la ventana anterior
-$(document).ready(function () {
-  $("#btnGuardarD").click(function () {
-    $("#miModalR").modal("show");
-  });
-  $("#btnGuardarD").click(function () {
-    $("#miModalP").modal("hide");
-  });
-});
-
-  //Ventana modal para mostrar la actualización de datos
-$(document).ready(function () {
-  $("#btnAAActualiz").click(function () {
-    $("#miModalAC").modal("show");
-  });
-  $("#btnAAActualiz").click(function () {
-    $("#miModalP2").modal("hide");
-  });
-});
-
-
-//Ventana modal para mostrar la eliminación de un datos
-
-$(document).ready(function () {
-  $("#btnElimin").click(function () {
-    $("#miModalDe2").modal("show");
-  });
-  $("#btnElimin").click(function () {
-    $("#miModalDe").modal("hide");
-  });
-});
-
-//Validaciones
-
-//Validación para que solamente se escriban letras, pero que pueda haber guiones y espacios
-$(document).ready(function() {
-  $("#nombreProducto").on("keypress", function(event) {
-      var inputValue = event.which;
-      // Permitir solo letras y algunos caracteres especiales como espacio, guión, etc.
-      if (!(inputValue >= 65 && inputValue <= 90) && // letras mayúsculas
-          !(inputValue >= 97 && inputValue <= 122) && // letras minúsculas
-          !(inputValue == 32 || inputValue == 45 || inputValue == 46 || inputValue == 44)) { // espacio, guión, punto, coma
-          event.preventDefault();
-      }
-  });
-});
-
-//Barra de busqueda para las cards
-document.addEventListener("DOMContentLoaded", function() {
-  const searchInput = document.getElementById("Search");
-  const noResultsElement = document.getElementById("noResults"); // Referencia al elemento del mensaje
-
-  searchInput.addEventListener("input", function() {
-      const searchText = searchInput.value.toLowerCase();
-      const cards = document.querySelectorAll(".card");
-      let found = false; // Indicador de si se encontró al menos una coincidencia
-
-      cards.forEach(card => {
-          const cardText = card.textContent.toLowerCase();
-          if (cardText.includes(searchText)) {
-              card.style.display = "";
-              found = true; // Marca como encontrada una coincidencia
-          } else {
-              card.style.display = "none";
-          }
-      });
-
-      // Muestra u oculta el mensaje de 'no resultados' basado en si se encontraron coincidencias
-      noResultsElement.style.display = found ? "none" : "block";
-  });
-});
-
-//Cambiar el estado
-document.addEventListener("DOMContentLoaded", function() {
-  const mostrarButtons = document.querySelectorAll(".btnMostrar");
-
-  mostrarButtons.forEach(button => {
-      button.addEventListener("click", function() {
-          // Asignar el contexto de cada botón a su card específica
-          const card = button.closest('.card');
-
-          // Buscar el label de 'Estado:' dentro de esta card
-          const estadoLabel = Array.from(card.querySelectorAll("label")).find(label => label.textContent.includes("Estado:"));
-          if (estadoLabel) {
-              const valueLabel = estadoLabel.nextElementSibling;
-
-              // Cambiar el texto dependiendo del estado actual
-              if (valueLabel.textContent.trim() === "En venta") {
-                  valueLabel.textContent = "No disponible";
-              } else {
-                  valueLabel.textContent = "En venta";
-              }
-          }
-      });
-  });
-});
-
-
-
-
-
