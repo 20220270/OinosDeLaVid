2795d7a8b5b9edbae4f72c67e20ec645df9e3784
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-13 10:54:35 -0600

    pequeño arrego

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,7 +1,51 @@
 const addForm = document.getElementById('sessionForm');
+const signUp = document.getElementById('modalRegistrasrte');
 const USER_API = 'services/admin/administrador.php';
 
-// Constantes para los componentes del modal de registro 
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    //loadTemplate();
+    // Petición para consultar los usuarios registrados.
+    const DATA = await fetchData(USER_API, 'readUsers');
+    // Se comprueba si existe una sesión, de lo contrario se sigue con el flujo normal.
+    if (DATA.session) {
+        // Se direcciona a la página web de bienvenida.
+        location.href = 'menu.html';
+    } else if (DATA.status) {
+        // Se establece el título del contenido principal.
+        //MAIN_TITLE.textContent = 'Iniciar sesión';
+        // Se muestra el formulario para iniciar sesión.
+        addForm.classList.remove('d-none');
+        sweetAlert(4, DATA.message, true);
+    } else {
+        // Se establece el título del contenido principal.
+        //MAIN_TITLE.textContent = 'Registrar primer usuario';
+        // Se muestra el formulario para registrar el primer usuario.
+        signUp.classList.remove('d-none');
+        sweetAlert(4, DATA.error, true);
+    }
+});
+
+
+// Método del evento para cuando se envía el formulario de registro del primer usuario.
+signUp.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(signUp);
+    // Petición para registrar el primer usuario del sitio privado.
+    const DATA = await fetchData(USER_API, 'signUp', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'index.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+
+// Constantes para los componentes del modal de inicio de sesion 
 
 
 addForm.addEventListener('submit', async(event)=>{
