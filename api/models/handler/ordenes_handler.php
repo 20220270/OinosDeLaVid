410208b1e0454482204a2851daa6cc257ee4440f
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-16 09:32:09 -0600

    hshshs

--- api/models/handler/ordenes_handler.php
+++ api/models/handler/ordenes_handler.php
@@ -9,6 +9,7 @@ class OrdenesHandler
     /*
      *  Declaración de atributos para el manejo de datos.
      */
+    protected $idpedido = null;
     protected $iddetalle = null;
     protected $nombrecliente = null;
     protected $estadoorden = null;
@@ -62,6 +63,94 @@ class OrdenesHandler
                 SET estado_orden = ?
                 WHERE id_detalle = ?';
         $params = array($this->estadoorden, $this->iddetalle);
+        return Database::executeRow($sql, $params);
+    }
+
+
+    /** */
+    public function getOrder()
+    {
+        $this->estadoorden = 'Pendiente';
+        $sql = 'SELECT id_detalle, id_orden
+                FROM tb_detallesOrdenes
+                INNER JOIN tb_ordenes USING(id_orden)
+                WHERE estado_pedido = ? AND id_cliente = ?';
+        $params = array($this->estadoorden, $_SESSION['idCliente']);
+        if ($data = Database::getRow($sql, $params)) {
+            $_SESSION['idPedido'] = $data['id_orden'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Método para iniciar un pedido en proceso.
+    public function startOrder()
+    {
+        if ($this->getOrder()) {
+            return true;
+        } else {
+            $sql = 'INSERT INTO tb_ordenes(direccion_pedido, id_cliente)
+                    VALUES((SELECT direccion_cliente FROM tb_clientes WHERE id_cliente = ?), ?)';
+            $params = array($_SESSION['idCliente'], $_SESSION['idCliente']);
+            // Se obtiene el ultimo valor insertado de la llave primaria en la tabla pedido.
+            if ($_SESSION['idPedido'] = Database::getLastRow($sql, $params)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Método para agregar un producto al carrito de compras.
+    public function createDetail()
+    {
+        // Se realiza una subconsulta para obtener el precio del producto.
+        $sql = 'INSERT INTO tb_detallesOrdenes(id_producto, precio_producto, cantidad_producto, id_orden)
+                VALUES(?, (SELECT precio_producto FROM tb_productos WHERE id_producto = ?), ?, ?)';
+        $params = array($this->producto, $this->producto, $this->cantidad, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para obtener los productos que se encuentran en el carrito de compras.
+    public function readDetail()
+    {
+        $sql = 'SELECT id_detalle, nombre_producto,precio_producto, cantidad_producto
+        FROM tb_detallesOrdenes
+        INNER JOIN tb_ordenes USING(id_orden)
+        INNER JOIN tb_productos USING(id_producto)
+                WHERE id_orden = ?';
+        $params = array($_SESSION['idPedido']);
+        return Database::getRows($sql, $params);
+    }
+
+    // Método para finalizar un pedido por parte del cliente.
+    public function finishOrder()
+    {
+        $this->estadoorden = 'Finalizado';
+        $sql = 'UPDATE tb_detallesOrdenes
+                SET estado_pedido = ?
+                WHERE id_orden = ?';
+        $params = array($this->estadoorden, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para actualizar la cantidad de un producto agregado al carrito de compras.
+    public function updateDetail()
+    {
+        $sql = 'UPDATE tb_detallesOrdenes
+                SET cantidad_producto = ?
+                WHERE id_detalle = ? AND id_orden = ?';
+        $params = array($this->cantidad, $this->iddetalle, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para eliminar un producto que se encuentra en el carrito de compras.
+    public function deleteDetail()
+    {
+        $sql = 'DELETE FROM tb_detallesOrdenes
+                WHERE id_detalle = ? AND id_orden = ?';
+        $params = array($this->iddetalle, $_SESSION['idPedido']);
         return Database::executeRow($sql, $params);
     }
 }
