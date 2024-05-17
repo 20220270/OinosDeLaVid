93fae7fbcc479cfc210f80f28f592fcfa6963976
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-17 07:48:41 -0600

    update de marcas

--- controllers/admin/marcas.js
+++ controllers/admin/marcas.js
@@ -3,9 +3,8 @@ const MARCA_API = 'services/admin/marca.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('searchForm');
 // Constantes para establecer los elementos de la tabla.
-const TABLE_BODY = document.getElementById('tableBody'),
-    ROWS_FOUND = document.getElementById('rowsFound')
-    //CARD_MARCAS = document.getElementById('cardMarcas');
+
+    CARD_MARCAS = document.getElementById('cardMarcas');
 // Constantes para establecer los elementos del componente Modal.
 const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
     MODAL_TITLE = document.getElementById('modalTitle');
@@ -28,93 +27,87 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Método del evento para cuando se envía el formulario de buscar.
 SEARCH_FORM.addEventListener('submit', (event) => {
-  // Se evita recargar la página web después de enviar el formulario.
-  event.preventDefault();
-  // Constante tipo objeto con los datos del formulario.
-  const FORM = new FormData(SEARCH_FORM);
-  // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
-  fillTable(FORM);
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
 });
 
 // Método del evento para cuando se envía el formulario de guardar.
 SAVE_FORM.addEventListener('submit', async (event) => {
-  // Se evita recargar la página web después de enviar el formulario.
-  event.preventDefault();
-  // Se verifica la acción a realizar.
-  (ID_MARCA.value) ? action = 'updateRow' : action = 'createRow';
-  // Constante tipo objeto con los datos del formulario.
-  const FORM = new FormData(SAVE_FORM);
-  // Petición para guardar los datos del formulario.
-  const DATA = await fetchData(MARCA_API, action, FORM);
-  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-  if (DATA.status) {
-      // Se cierra la caja de diálogo.
-      SAVE_MODAL.hide();
-      // Se muestra un mensaje de éxito.
-      sweetAlert(1, DATA.message, true);
-      // Se carga nuevamente la tabla para visualizar los cambios.
-      fillTable();
-  } else {
-      sweetAlert(2, DATA.error, false);
-  }
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_MARCA.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(MARCA_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
 });
 
 const fillTable = async (form = null) => {
-  // Se inicializa el contenido de la tabla.
-  ROWS_FOUND.textContent = '';
-  TABLE_BODY.innerHTML = '';
-  //CARD_MARCAS.innerHTML = '';
-  // Se verifica la acción a realizar.
-  (form) ? action = 'searchRows' : action = 'readAll';
-  // Petición para obtener los registros disponibles.
-  const DATA = await fetchData(MARCA_API, action, form);
-  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-  if (DATA.status) {
-      console.log(DATA)
-      // Se recorre el conjunto de registros fila por fila.
-      DATA.dataset.forEach(row => {
-          // Se crean y concatenan las filas de la tabla con los datos de cada registro.
-          TABLE_BODY.innerHTML += `
-              <tr>
-                  <td>${row.id_marca}</td>
-                  <td>${row.nombre_marca}</td>
-                  <td><img src="${SERVER_URL}images/marcas/${row.logo_marca}" height="50"></td>
-                  <td>
-                      
-                  
+    // Se inicializa el contenido de la tabla.
+    
+    CARD_MARCAS.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(MARCA_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        console.log(DATA)
+        // Se recorre el conjunto de registros fila por fila.
+        DATA.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            CARD_MARCAS.innerHTML += `
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4 mt-5 text-center">
+                <div class="card h-100">
+                    <img src="${SERVER_URL}images/marcas/${row.logo_marca}" class="card-img-top" height="200px" width="200px">
+                    <div class="card-body">
+                        <h5 class="card-title">${row.nombre_marca}</h5>
+                    </div>
+                </div>
 
-                    
-                    <button type="submit" class="btn btn-success mt-1" id="btnEliminar" name="btnEliminar" onclick="openDelete(${row.id_marca})">
+                <button type="submit" class="btn btn-success mt-1 " id="btnEliminar" name="btnEliminar" onclick="openDelete(${row.id_marca})">
                         <i class="bi bi-search"></i>
                         <img src="../../resources/Imagenes/btnEliminarIMG.png" alt="" width="30px" height="30px"
-                            class="mb-1">
+                            class="mb-4">
 
                     </button>
                     <button type="reset" class="btn btn-secondary mt-1" id="btnActualizar" name="btnActualizar" onclick="openUpdate(${row.id_marca})">
                         <i class="bi bi-x-square-fill"></i>
                         <img src="../../resources/Imagenes/btnActualizarIMG.png" alt="" width="30px" height="30px"
-                            class="mb-1">
+                            class="mb-4">
                     </button>
-
-
-                  </td>
-              </tr>
+            </div>
           `;
-          
-      });
-      // Se muestra un mensaje de acuerdo con el resultado.
-      ROWS_FOUND.textContent = DATA.message;
-  } else {
-      sweetAlert(4, DATA.error, true);
-  }
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
 }
 
 const openCreate = () => {
-  // Se muestra la caja de diálogo con su título.
-  SAVE_MODAL.show();
-  MODAL_TITLE.textContent = 'Registrar marca';
-  // Se prepara el formulario.
-  SAVE_FORM.reset();
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Registrar marca';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
 }
 
 /*
@@ -123,26 +116,26 @@ const openCreate = () => {
 *   Retorno: ninguno.
 */
 const openUpdate = async (id) => {
-  // Se define una constante tipo objeto con los datos del registro seleccionado.
-  const FORM = new FormData();
-  FORM.append('idMarca', id);
-  // Petición para obtener los datos del registro solicitado.
-  const DATA = await fetchData(MARCA_API, 'readOne', FORM);
-  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-  if (DATA.status) {
-      // Se muestra la caja de diálogo con su título.
-      SAVE_MODAL.show();
-      MODAL_TITLE.textContent = 'Actualizar marca';
-      // Se prepara el formulario.
-      SAVE_FORM.reset();
-      // Se inicializan los campos con los datos.
-      const ROW = DATA.dataset;
-      ID_MARCA.value = ROW.id_marca;
-      NOMBRE_MARCA.value = ROW.nombre_marca;
-      LOGO_MARCA.value = ROW.logo_marca;
-  } else {
-      sweetAlert(2, DATA.error, false);
-  }
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idMarca', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(MARCA_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar marca';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_MARCA.value = ROW.id_marca;
+        NOMBRE_MARCA.value = ROW.nombre_marca;
+        LOGO_MARCA.value = ROW.logo_marca;
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
 }
 
 /*
@@ -151,25 +144,25 @@ const openUpdate = async (id) => {
 *   Retorno: ninguno.
 */
 const openDelete = async (id) => {
-  // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-  const RESPONSE = await confirmAction('¿Desea eliminar la MARCA de forma permanente?');
-  // Se verifica la respuesta del mensaje.
-  if (RESPONSE) {
-      // Se define una constante tipo objeto con los datos del registro seleccionado.
-      const FORM = new FormData();
-      FORM.append('idMarca', id);
-      // Petición para eliminar el registro seleccionado.
-      const DATA = await fetchData(MARCA_API, 'deleteRow', FORM);
-      // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-      if (DATA.status) {
-          // Se muestra un mensaje de éxito.
-          await sweetAlert(1, DATA.message, true);
-          // Se carga nuevamente la tabla para visualizar los cambios.
-          fillTable();
-      } else {
-          sweetAlert(2, DATA.error, false);
-      }
-  }
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar la MARCA de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idMarca', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(MARCA_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
 }
 
 
