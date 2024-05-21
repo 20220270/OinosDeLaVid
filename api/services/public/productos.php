8a9fdd0f389314d62c75f542697753f940fdde56
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-20 19:25:59 -0600

    servicios de categorias y productos

--- api/services/public/productos.php
+++ api/services/public/productos.php
@@ -1,20 +1,31 @@
 <?php
 // Se incluye la clase del modelo.
-require_once('../../models/data/categoria_data.php');
+require_once('../../models/data/producto_data.php');
 
 // Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
 if (isset($_GET['action'])) {
     // Se instancia la clase correspondiente.
-    $categoria = new CategoriaData;
+    $producto = new ProductoData;
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null);
     // Se compara la acción a realizar según la petición del controlador.
     switch ($_GET['action']) {
-        case 'readAll':
-            if ($result['dataset'] = $categoria->readAll()) {
+        case 'readProductosCategoria':
+            if (!$producto->setCategoria($_POST['idCategoria'])) {
+                $result['error'] = $producto->getDataError();
+            } elseif ($result['dataset'] = $producto->readProductosCategoria()) {
                 $result['status'] = 1;
             } else {
-                $result['error'] = 'No existen categorías para mostrar';
+                $result['error'] = 'No existen productos para mostrar';
+            }
+            break;
+        case 'readOne':
+            if (!$producto->setId($_POST['idProducto'])) {
+                $result['error'] = $producto->getDataError();
+            } elseif ($result['dataset'] = $producto->readOne()) {
+                $result['status'] = 1;
+            } else {
+                $result['error'] = 'Producto inexistente';
             }
             break;
         default:
