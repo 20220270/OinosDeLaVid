ed2df271195bbb383914856931ed858bf3cc4cfa
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-20 20:45:22 -0600

    arr

--- controllers/public/actuSignUp.js
+++ controllers/public/actuSignUp.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('#btnGuardarAcCo');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            // Cerrar la modal ModalActualizarContraseña
-            const modalActualizarContraseña = bootstrap.Modal.getInstance(document.getElementById('ModalActualizarContraseña'));
-            modalActualizarContraseña.hide();
 
-            // Mostrar la modal modalContraCtualizada
-            const modalContraCtualizada = new bootstrap.Modal(document.getElementById('modalContraCtualizada'));
-            modalContraCtualizada.show();
-        });
-    });
-});
