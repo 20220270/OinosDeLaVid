999ae4efc2d8f4cc24ac93fb4d4ad9ebc7ac1ab5
20220270 <136751152+20220270@users.noreply.github.com>
2024-05-23 15:22:10 -0600

    actualizacion de los productos y boton para los comentarios

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -22,20 +22,27 @@ document.addEventListener('DOMContentLoaded', async () => {
         PRODUCTOS.innerHTML = '';
         // Se recorre el conjunto de registros fila por fila a través del objeto row.
         DATA.dataset.forEach(row => {
+            // Convertir el valor del descuento a porcentaje, y agregando un estilo unicamente al texto del descuento, no a todo el campo li en el que se encuentra
+            let descuento = row.descuento_producto ? `<span class="descuento">${row.descuento_producto}% de descuento</span>` : 'Sin descuento';
+
             // Se crean y concatenan las tarjetas con los datos de cada producto.
             PRODUCTOS.innerHTML += `
                 <div class="col-sm-12 col-md-6 col-lg-3">
                     <div class="card mb-3">
                         <img src="${SERVER_URL}images/productos/${row.imagen_producto}" class="card-img-top" alt="${row.nombre_producto}">
                         <div class="card-body">
-                            <h5 class="card-title">${row.nombre_producto}</h5>
+                            <h5 class="card-title text-center">${row.nombre_producto}</h5>
                         </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Precio unitario (US$) ${row.precio_producto}</li>
-                            <li class="list-group-item">Existencias ${row.existencias_producto}</li>
+                            <li class="list-group-item etqiueta">Precio unitario (US$) ${row.precio_producto}</li>
+                            <li class="list-group-item">Descripción: ${row.descripcion_producto}</li>
+                            <li class="list-group-item etqiueta">Existencias ${row.existencias_producto}</li>
+                            <li class="list-group-item  text-center mt-4 mb-4">${descuento}</li>
                         </ul>
                         <div class="card-body text-center">
-                            <a href="detalleproducto.html?id=${row.id_producto}" class="btn btn-primary">Ver detalle</a>
+                            <a href="detalleproducto.html?id=${row.id_producto}" class="btn" id="btnVerDetalle">Ver detalle</a>
+                            <a class="btn"><img src="../../resources/Imagenes/comentarios.png" class="mt-1" alt="..."
+                            width="30px" height="30px"></a>
                         </div>
                     </div>
                 </div>
