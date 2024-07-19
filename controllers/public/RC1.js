ade508e6cc8a9f94e446ab627c6dab07508be402
adianitafresita <marhernanndezz@gmail.com>
2024-07-19 15:31:24 -0600

    Intento del correo 1

--- controllers/public/RC1.js
+++ controllers/public/RC1.js
@@ -1,3 +1,28 @@
+
+function validarFormulario(event) {
+  // Prevenir el envío del formulario si hay errores
+  event.preventDefault();
+
+  // Obtener el valor del campo de correo electrónico
+  var correo = document.getElementById("IngreseCorreo").value.trim();
+
+  // Expresión regular para validar formato de correo electrónico
+  var correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Verificar si el correo está lleno y tiene un formato válido
+  if (correo === "") {
+      alert("Por favor, ingresa tu correo electrónico.");
+      return;
+  } 
+  
+  if (!correoRegex.test(correo)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      return;
+  }
+
+  // Si el correo es válido, redirigir al usuario a recuperarcontraseña_codigo.html
+  window.location.href = 'recuperarcontraseña_codigo.html';
+}
 document.getElementById("btnVo").addEventListener("click", function () {
     location.href = "login.html";
   });
